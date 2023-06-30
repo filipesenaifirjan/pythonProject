322e6e6f9ebc4bd9d5d6ade3e79270108fbd17e5
filipesenaifirjan <filipemattossenai@gmail.com>
2023-06-30 19:20:57 -0300

    primitivos

---
+++ desafio01.py
@@ -0,0 +1,6 @@
+#crie um programa que recebe um numero e imprima o sucessor e o antecessor
+
+n1 = int(input('digite um número: '))
+n2 = n1 - 1
+n3 = n1 + 1
+print('O sucessor é: ', n3, 'E o antecessor é: ', n2)
