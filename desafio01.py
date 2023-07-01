776ab8fd61513020927201b70d246c7709df6e27
filipesenaifirjan <filipemattossenai@gmail.com>
2023-07-01 18:55:01 -0300

    bee1038

--- desafio01.py
+++ desafio01.py
@@ -4,3 +4,5 @@ n1 = int(input('digite um número: '))
 n2 = n1 - 1
 n3 = n1 + 1
 print('O sucessor é: ', n3, 'E o antecessor é: ', n2)
+#outra maneira de fazer é:
+# n = int(input('digite um numero')) print('Analisando o valor {}, seu antecessor é {} e seu sucessor é {} .format(n, (n-1), (n+1)))
