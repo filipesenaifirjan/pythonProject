9370002ffec1b5f4414b1e5ff6f03cdab93d25aa
Filipe de Mattos Assunção <89521521+filipesenaifirjan@users.noreply.github.com>
2023-08-14 18:32:01 -0300

    2187

---
+++ beecrow2187.py
@@ -0,0 +1,30 @@
+#byfilipedev
+
+'''
+Bits Trocados
+'''
+
+test = 0
+
+while True:
+
+    test += 1
+
+    value = int(input())
+
+    if value == 0:
+        break
+
+    notes_50 = value // 50
+    value -= 50 * notes_50
+
+    notes_10 = value // 10
+    value -= 10 * notes_10
+
+    notes_5 = value // 5
+    value -= 5 * notes_5
+
+    notes_1 = value
+
+    print(f"Teste {test}")
+    print(f"{notes_50} {notes_10} {notes_5} {notes_1}\n")
