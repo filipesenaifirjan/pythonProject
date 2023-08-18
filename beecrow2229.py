6c4aad00b1bdcdc382f6116432d449ef12cc98b9
Filipe de Mattos Assunção <89521521+filipesenaifirjan@users.noreply.github.com>
2023-08-18 19:12:25 -0300

    2229

---
+++ beecrow2229.py
@@ -0,0 +1,15 @@
+#byfilipedev
+test = 0
+
+while True:
+
+    test += 1
+
+    D = int(input())
+
+    if D == -1:
+        break
+
+    print("Teste %d" % test)
+    print((2 ** D - 1) ** 2 + 4 * (2 ** D - 1) + 4)
+    print("")
