e4a226acab5930513d1a4a072afc8b5f4f1e8662
Filipe de Mattos Assunção <89521521+filipesenaifirjan@users.noreply.github.com>
2023-08-19 15:52:03 -0300

    2366

---
+++ beecrow2366.py
@@ -0,0 +1,26 @@
+# filipedev
+
+nP, iD = map(int, input().split())
+dP = list(map(int, input().split()))
+
+f = True
+
+for i in range(nP):
+
+    if i != nP - 1:
+
+        if dP[i + 1] - dP[i] > iD:
+
+            f = False
+    else:
+
+        if 42195 - dP[i] > iD:
+
+            f = False
+
+if f:
+
+    print("S")
+else:
+
+    print("N")
