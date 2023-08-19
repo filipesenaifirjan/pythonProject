f201893eb7bc75ce8de0af6f7b7c99cda0f408af
Filipe de Mattos Assunção <89521521+filipesenaifirjan@users.noreply.github.com>
2023-08-19 16:40:15 -0300

    2395

---
+++ beecrow2395.py
@@ -0,0 +1,10 @@
+# by filipedev
+
+a, b, c = map(int, input().split())
+x, y, z = map(int, input().split())
+
+horizontalAxisMax = x // a
+longitudinalAxisMax = y // b
+verticalAxisMax = z // c
+
+print(horizontalAxisMax * longitudinalAxisMax * verticalAxisMax)
