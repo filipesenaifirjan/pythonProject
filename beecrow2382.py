35fbbb8f68585153e16949aed3c0e91aaae75dab
Filipe de Mattos Assunção <89521521+filipesenaifirjan@users.noreply.github.com>
2023-08-19 16:03:52 -0300

    2382

---
+++ beecrow2382.py
@@ -0,0 +1,14 @@
+# by fil codes
+
+from math import sqrt
+
+nStr = input().split()
+nMap = map(int, nStr)
+nLst = list(nMap)
+
+if sqrt(nLst[0]**2 + nLst[1]**2 + nLst[2]**2) / 2 <= nLst[3]:
+
+    print("S")
+else:
+
+    print("N")
