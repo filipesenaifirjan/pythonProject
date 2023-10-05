3a0a4448d565c396002c6fe7d663b5c3892928d9
Filipe de Mattos Assunção <89521521+filipesenaifirjan@users.noreply.github.com>
2023-10-05 12:17:57 -0300

    1800.py

---
+++ beecrow1800.py
@@ -0,0 +1,10 @@
+# encoding : utf-8
+q, e = [int(i) for i in input().split(" ")]
+jaesteve = [int(k) for k in input().split(" ")]
+for l in range(q):
+    agora = int(input())
+    if agora in jaesteve:
+        print(0)
+    else:
+        print(1)
+        jaesteve.append(agora)
