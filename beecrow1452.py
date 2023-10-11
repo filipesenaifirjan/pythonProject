7e2d9827ee150de433d617ecb7f927c0be9d4d88
Filipe de Mattos Assunção <89521521+filipesenaifirjan@users.noreply.github.com>
2023-10-11 12:11:36 -0300

    1452

---
+++ beecrow1452.py
@@ -0,0 +1,16 @@
+while True:
+    n, m = [int(i) for i in input().split()]
+    if n == 0 and m == 0:
+        break
+    servidores = []
+    usuarios = []
+    conexoes = 0
+    for i in range(n):
+        servidores.append(set(input().split()[1:]))
+    for i in range(m):
+        usuarios.append(set(input().split()[1:]))
+    for i in usuarios:
+        for j in servidores:
+            if j.intersection(i):
+                conexoes += 1
+    print(conexoes)
