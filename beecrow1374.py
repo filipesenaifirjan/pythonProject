bf44bbb3bb27e65c2ca5326147cd2b02fa39821f
Filipe de Mattos Assunção <89521521+filipesenaifirjan@users.noreply.github.com>
2023-10-12 21:00:20 -0300

    1374

---
+++ beecrow1374.py
@@ -0,0 +1,19 @@
+import datetime
+
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    resp = 0
+    cont = 0
+    lista1 = []
+    lista2 = []
+    for i in range(n):
+        a, b, c, d = [int(i) for i in input().split()]
+        lista1.append(datetime.datetime(c, b, a))
+        lista2.append(d)
+    for i in range(1, n):
+        if lista1[i] - datetime.timedelta(days=1) == lista1[i - 1]:
+            cont += 1
+            resp += lista2[i] - lista2[i - 1]
+    print("%d %d" % (cont, resp))
