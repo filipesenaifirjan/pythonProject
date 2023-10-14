0a6a18a7f2f0ad0b24716ab6b45016870d7832a3
Filipe de Mattos Assunção <89521521+filipesenaifirjan@users.noreply.github.com>
2023-10-14 15:16:14 -0300

    1309

---
+++ beecrow1309.py
@@ -0,0 +1,27 @@
+def metodo(texto):
+    if texto in [str(i) for i in range(10)]:
+        return "0%s" % texto
+    else:
+        return texto
+
+
+while True:
+    try:
+        entrada1 = [k for k in input()]
+        entrada2 = input()
+        tamanho = len(entrada1)
+        if tamanho <= 3:
+            print("$%s.%s" % ("".join(entrada1), metodo(entrada2)))
+        else:
+            if tamanho % 3 == 1:
+                entrada1.insert(0, "*")
+                entrada1.insert(0, "*")
+            elif tamanho % 3 == 2:
+                entrada1.insert(0, "*")
+            count = 0
+            for i in range(3, tamanho, 3):
+                entrada1.insert(i + count, ",")
+                count += 1
+            print("$%s.%s" % ("".join(entrada1).replace("*", ""), metodo(entrada2)))
+    except EOFError:
+        break
