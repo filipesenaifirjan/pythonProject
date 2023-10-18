b338182077d5717baab728666e52250ac1c418ff
Filipe de Mattos Assunção <89521521+filipesenaifirjan@users.noreply.github.com>
2023-10-18 10:12:54 -0300

    1082

---
+++ beecrow1082.py
@@ -0,0 +1,27 @@
+# -*- coding : utf-8 -*-  by filcodes
+casos = int(input())
+alfabeto = "abcdefghijklmnopqrstuvwxyz"
+for caso_da_vez in range(casos):
+    vertices, arestas = [int(i) for i in input().split()]
+    letras = alfabeto[:vertices]
+    grafo = {x: [] for x in letras}
+    jaesteve = {x: 1 for x in letras}
+    for aresta_da_vez in range(arestas):
+        componente1, componente2 = input().split()
+        grafo[componente1].append(componente2)
+        grafo[componente2].append(componente1)
+    print("Case #%d:" % (caso_da_vez + 1))
+    resposta = 0
+    for letra in letras:
+        if jaesteve[letra]:
+            resposta += 1
+            caminho = []
+            pilha = [letra]
+            while pilha:
+                v = pilha.pop()
+                if jaesteve[v]:
+                    caminho.append(v + ",")
+                    pilha.extend(grafo[v])
+                    jaesteve[v] = 0
+            print("".join(sorted(caminho)))
+    print("%d connected components\n" % resposta)
